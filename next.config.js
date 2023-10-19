8dd25a52d6dc1cb5a5b6a9f72f7d93bddad8904a
kohit123 <phanxuansy219@gmail.com>
2023-10-20 01:10:29 +0700

    config: Setup i18n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withNextIntl = require('next-intl/plugin')(
+  './i18n.ts'
+);
 
-module.exports = nextConfig
+const nextConfig = {
+}
+
+module.exports = withNextIntl({
+  ...nextConfig
+})
