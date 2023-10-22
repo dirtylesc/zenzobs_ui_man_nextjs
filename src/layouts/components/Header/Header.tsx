35f6c1d71481dcec7b3082edcdab714457c632d6
kohit123 <phanxuansy219@gmail.com>
2023-10-22 23:43:29 +0700

    views: Show Menu

--- src/layouts/components/Header/Header.tsx
+++ src/layouts/components/Header/Header.tsx
@@ -1,41 +1,92 @@
-import classNames from 'classnames';
+import classNames     from 'classnames';
+import { 
+  mdiBellOutline, 
+  mdiCogOutline
+}                     from '@mdi/js';
 
-import styles     from './Header.module.scss'
-import MenuList   from '@/components/Menu/SidebarMenu';
-import MenuListItem from '@/components/Menu/SidebarMenu/MenuListItem';
-import Image from 'next/image';
+import styles         from './Header.module.scss'
+import MenuList       from '@/components/Menu/SidebarMenu';
+import MenuListItem   from '@/components/Menu/SidebarMenu/MenuListItem';
 
 const cx        = classNames.bind(styles);
 const rightMenu = [
   {
-    key: "nationality",
-    title: "vn_language",
-    icon: {
-      src: "/public/icons/",
-      alt: "vn_language"
+    key     : "nationality",
+    title   : "cms.Lang.vi",
+    leftIcon: {
+      src   : "/icons/vn-circle.svg",
+      alt   : "cms.Lang.vi"
+    },
+    style: {
+      wrapper : styles.menu_list_item,
+      icon : styles.menu_list_item__icon
     }
   },
   {
-    key: 2
+    key     : "notification",
+    leftIcon: {
+      path: mdiBellOutline
+    },
+    style: {
+      wrapper : styles.menu_list_item,
+      icon : styles.menu_list_item__icon
+    }
+  },
+  {
+    key     : "setting",
+    leftIcon: {
+      path  : mdiCogOutline
+    },
+    style: {
+      wrapper : styles.menu_list_item,
+      icon : styles.menu_list_item__icon
+    }
+  },
+  {
+    key: "user",
+    title: "cms.def_name",
+    leftIcon: {
+      src   : "/images/avatar-1.jpg",
+      alt   : "cms.Lang.vi"
+    },
+    style: {
+      wrapper: styles.menu_list_item_user,
+      icon : "rounded-full",
+    }
   }
 ] 
 const styleObj  = {
-  wrapper: [styles.wrapper],
-  rightMenu: [
-    styles.right_menu, 
+  wrapper   : [
+    styles.wrapper
+  ],
+  rightMenu : [
+    styles.right_menu,
     "float-right", 
-    "mb-0", 
+    "mb-0",
     "list-none"
-  ]
+  ],
+  item      : {
+    show    : styles.menu_list_item__show,
+    title   : styles.menu_list_item__title
+  }
 }
 
 function Header() {
   return (
-    <header className={cx(...styleObj.wrapper)}>
-      <MenuList style={{ list: cx(...styleObj.rightMenu) }}>
+    <header className = {cx(...styleObj.wrapper)}>
+      <MenuList style = {{ list: cx(...styleObj.rightMenu) }}>
         {
           rightMenu.map(item => 
-            <MenuListItem key={item.key}/>
+            <MenuListItem 
+              key       = {item.key} 
+              title     = {item.title} 
+              leftIcon  = {item.leftIcon}
+              style     = {{ 
+                ...styleObj.item,
+                wrapper : item.style.wrapper,
+                icon    : item.style.icon 
+              }}
+            />
           )
         }
       </MenuList>
