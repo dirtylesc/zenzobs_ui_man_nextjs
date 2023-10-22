771e782c81897f93856ec5e66d4b931e2540ab22
kohit123 <phanxuansy219@gmail.com>
2023-10-22 23:45:10 +0700

    components: Create Ent + List

---
+++ src/components/List/List.tsx
@@ -0,0 +1,11 @@
+import classNames     from 'classnames';
+
+import styles         from './List.module.scss';
+
+const cx        = classNames.bind(styles);
+
+function List() {
+    
+}
+
+export default List;
