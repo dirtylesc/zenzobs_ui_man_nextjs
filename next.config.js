ee7c426f87d0a19497cd1d6c3baea264f53bd505
kohit123 <phanxuansy219@gmail.com>
2023-10-25 00:44:59 +0700

    configs: jquery

--- next.config.js
+++ next.config.js
@@ -2,8 +2,19 @@
 const withNextIntl = require('next-intl/plugin')(
   './i18n.ts'
 );
+const webpack = require('webpack');
 
 const nextConfig = {
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $     : 'jquery',
+        jQuery: 'jquery'
+      })
+    );
+
+    return config;
+  },
 }
 
 module.exports = withNextIntl({
